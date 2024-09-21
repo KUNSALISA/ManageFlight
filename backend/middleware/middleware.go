b605af9b03e1d51ca1bc60bebad77beada347b82
KUNSALISA <attawerapathsalisa@gmail.com>
2024-09-21 15:39:54 +0700

    _

--- backend/middleware/middleware.go
+++ backend/middleware/middleware.go
@@ -31,38 +31,5 @@ func AuthMiddleware() gin.HandlerFunc {
 	}
 }
 
-// package middleware
 
-// import (
-// 	"net/http"
-
-// 	"github.com/KUNSALISA/ManageFlight/services"
-// 	_"github.com/dgrijalva/jwt-go"
-// 	"github.com/gin-gonic/gin"
-// )
-
-// func AuthMiddleware() gin.HandlerFunc {
-// 	return func(c *gin.Context) {
-// 		tokenString := c.GetHeader("Authorization")
-// 		if tokenString == "" {
-// 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Token is missing"})
-// 			c.Abort()
-// 			return
-// 		}
-
-// 		var jwtWrapper services.JwtWrapper
-// 		jwtWrapper.SecretKey = "Manage" // SecretKey ของคุณ
-// 		jwtWrapper.Issuer = "ManageFlight"
-
-// 		claims, err := jwtWrapper.ValidateToken(tokenString)
-// 		if err != nil {
-// 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
-// 			c.Abort()
-// 			return
-// 		}
-
-// 		c.Set("email", claims.Email)
-// 		c.Next()
-// 	}
-// }
 
